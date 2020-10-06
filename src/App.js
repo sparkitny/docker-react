554ae42dd0d48552afd31d3c2366c31fa56f362a
Sebastian Parkitny <sebastian.parkitny@digitalacceleratorplatform.com>
2020-10-06 18:00:59 +0200

    changes

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hallo, wie geht's?
+          Hallo, wie geht's? Gut, oder? Oder doch nicht?
         </p>
         <a
           className="App-link"
